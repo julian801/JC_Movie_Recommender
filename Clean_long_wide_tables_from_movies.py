9a5d6b45de2c60195b7eb0e211059b42e406c93b
lyonne_19 <35970117+lyonne19@users.noreply.github.com>
2018-10-19 11:01:07 +0200

    movie csv x2 in place

--- Clean_long_wide_tables_from_movies.py
+++ Clean_long_wide_tables_from_movies.py
@@ -1,3 +1,9 @@
+# write simple ui
+# print explanatory text
+# ask user for genre
+# and fav reviewer
+# either can be empty
+# return the best scoring hit
 
 import pandas as pd
 
@@ -13,3 +19,26 @@ long = clean.set_index(['Name', 'Genre', 'Reviewer'])
 
 # mean rating per genre
 print(long.unstack(1).mean())
+
+print("""
+
+    A-Team recommender: Let BA find you a movie, fool!
+    
+    Enter your favourite genre of reviewier:
+
+""")
+
+print("enter a genre (or press enter to skip): ", end="")
+genre = imput()
+
+print("enter a reviewer (or press enter to skip): ", end="")
+reviewer = imput()
+
+if genre: #boolean evaluating whether the genre is null or not
+    gdf = df[df['genre']==genre]
+else:
+    g = df
+
+result = g.sort_values('Rating', ascending=False, inplace=True)
+print(g.head(3).sample(1))
+
