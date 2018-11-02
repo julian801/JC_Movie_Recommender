101b4bbcf0aff6da1313bbc9790c0136767cb049
Glynis Mattheisen <gmattheisen@gmail.com>
2018-11-02 13:31:58 +0100

    update

--- Very_Respectable_Code.py
+++ Very_Respectable_Code.py
@@ -3,7 +3,7 @@
 
 # ## NMF = Not Monday night Football !
 
-# In[100]:
+# In[1]:
 
 
 import pandas as pd
@@ -24,7 +24,7 @@ import csv
 
 # # User Input
 
-# In[101]:
+# In[2]:
 
 
 #importing ratings and movies csv files
@@ -33,14 +33,14 @@ PATH3 = "movies.csv"
 ratings, movies_ind = pd.read_csv(PATH2), pd.read_csv(PATH3)
 
 
-# In[102]:
+# In[3]:
 
 
 # create an empty array the length of number of movies in system
 user_ratings = np.zeros(9724)
 
 
-# In[103]:
+# In[4]:
 
 
 #format ratings dataframe
@@ -49,26 +49,26 @@ ratings.set_index(['userId','movieId'], inplace=True)
 ratings = ratings.unstack(0)
 
 
-# In[104]:
+# In[5]:
 
 
 ratings_count = ratings.count(axis=1) #count the number of ratings for each movie as a measure of popularity
 top = pd.DataFrame(ratings_count.sort_values(ascending = False).head(10)) #create a dataframe of the top 20 most popular movies
 
 
-# In[105]:
+# In[6]:
 
 
 top.reset_index(inplace=True)
 
 
-# In[106]:
+# In[7]:
 
 
 movies_ind.set_index('movieId',inplace=True)
 
 
-# In[107]:
+# In[8]:
 
 
 top_movies_g = movies_ind.loc[top['movieId']]['title'].values
@@ -77,7 +77,7 @@ top_movies_g = movies_ind.loc[top['movieId']]['title'].values
 # ## Of the following movies, rate all that you have seen on a scale of 1-5. 
 # ## If you have not seen a movie, rate 0.
 
-# In[139]:
+# In[9]:
 
 
 #creates a list of ratings for the prompted movies
@@ -91,25 +91,25 @@ for i in range(0,10):
     user_input.append(answer)
 
 
-# In[109]:
+# In[10]:
 
 
 movies_ind.reset_index(inplace=True)
 
 
-# In[110]:
+# In[11]:
 
 
 top_movies_index = movies_ind.index[top['movieId']].values
 
 
-# In[111]:
+# In[12]:
 
 
 top_movies_index
 
 
-# In[112]:
+# In[13]:
 
 
 # inputs user rating into large array (9,000+ count) at appropriate indexes
@@ -119,7 +119,7 @@ for i in range(0,10):
 
 # # NMF Modeling
 
-# In[113]:
+# In[14]:
 
 
 PATH4 = "movies.csv"
@@ -130,13 +130,13 @@ ratings = ratings["rating"]
 ratings = ratings.transpose()
 
 
-# In[114]:
+# In[15]:
 
 
 ratings.head(2)
 
 
-# In[115]:
+# In[16]:
 
 
 R = pd.DataFrame(ratings)
@@ -148,55 +148,55 @@ P = model.components_  # Movie feature
 Q = model.transform(R)  # User features
 
 
-# In[116]:
+# In[17]:
 
 
 query = user_ratings.reshape(1,-1)
 
 
-# In[117]:
+# In[18]:
 
 
 t=model.transform(query)
 
 
-# In[118]:
+# In[19]:
 
 
 outcome = np.dot(t,P)
 
 
-# In[119]:
+# In[20]:
 
 
 outcome=pd.DataFrame(outcome)
 
 
-# In[120]:
+# In[21]:
 
 
 outcome = outcome.transpose()
 
 
-# In[121]:
+# In[22]:
 
 
 outcome['movieId'] = movies_ind['movieId']
 
 
-# In[122]:
+# In[23]:
 
 
 outcome = outcome.rename(columns={0:'rating'})
 
 
-# In[123]:
+# In[24]:
 
 
 outcome
 
 
-# In[124]:
+# In[25]:
 
 
 top = outcome.sort_values(by='rating',ascending=False).head(100)
@@ -204,7 +204,7 @@ top = outcome.sort_values(by='rating',ascending=False).head(100)
 
 # # Selecting a Movie
 
-# In[125]:
+# In[26]:
 
 
 top_movie_recs = movies_ind.loc[top['movieId']]['title'].values
@@ -212,7 +212,7 @@ top_movie_recs = movies_ind.loc[top['movieId']]['title'].values
 
 # # Selecting a Movie with Genre Input
 
-# In[126]:
+# In[27]:
 
 
 #importing genres
@@ -220,279 +220,90 @@ PATHG = "movie_genres_years.csv"
 movie_genres = pd.read_csv(PATHG)
 
 
-# In[127]:
+# In[28]:
 
 
-# list of all movie Ids belonging to certain genres
-adventure_movies = list(movie_genres.loc[movie_genres['Genre_Adventure'] == 1]['movieId'])
-animation_movies= list(movie_genres.loc[movie_genres['Genre_Animation'] == 1]['movieId'])
-children_movies= list(movie_genres.loc[movie_genres['Genre_Children'] == 1]['movieId'])
-comedy_movies= list(movie_genres.loc[movie_genres['Genre_Comedy'] == 1]['movieId'])
-fantasy_movies= list(movie_genres.loc[movie_genres['Genre_Fantasy'] == 1]['movieId'])
-romance_movies= list(movie_genres.loc[movie_genres['Genre_Romance'] == 1]['movieId'])
-drama_movies= list(movie_genres.loc[movie_genres['Genre_Drama'] == 1]['movieId'])
-action_movies= list(movie_genres.loc[movie_genres['Genre_Action'] == 1]['movieId'])
-crime_movies= list(movie_genres.loc[movie_genres['Genre_Crime'] == 1]['movieId'])
-thriller_movies= list(movie_genres.loc[movie_genres['Genre_Thriller'] == 1]['movieId'])
-horror_movies= list(movie_genres.loc[movie_genres['Genre_Horror'] == 1]['movieId'])
-mystery_movies= list(movie_genres.loc[movie_genres['Genre_Mystery'] == 1]['movieId'])
-scifi_movies= list(movie_genres.loc[movie_genres['Genre_Sci-Fi'] == 1]['movieId'])
-war_movies= list(movie_genres.loc[movie_genres['Genre_War'] == 1]['movieId'])
-musical_movies= list(movie_genres.loc[movie_genres['Genre_Musical'] == 1]['movieId'])
-documentary_movies= list(movie_genres.loc[movie_genres['Genre_Documentary'] == 1]['movieId'])
-imax_movies= list(movie_genres.loc[movie_genres['Genre_IMAX'] == 1]['movieId'])
-western_movies= list(movie_genres.loc[movie_genres['Genre_Western'] == 1]['movieId'])
-noir_movies= list(movie_genres.loc[movie_genres['Genre_Film-Noir'] == 1]['movieId'])
-
-
-# In[128]:
-
-
+# creates list of genres
 genres = movie_genres.columns.values[3:22]
 
 
-# In[129]:
+# In[29]:
 
 
-a = {}
+# dictionary with keys equal to genre
+c = {}
 for x in genres:
     key = x
     value = ''
-    a[key] = value 
+    c[key] = value 
 
 
-# In[130]:
+# In[30]:
 
 
-ad = []
-an = []
-ch = []
-co = []
-fa = []
-ro = []
-dr = []
-ac = []
-cr = []
-th = []
-ho = []
-my = []
-sc = []
-wa = []
-mu = []
-do = []
-im = []
-we = []
-fi = []
-
-for x in top['movieId']:
-    if x in adventure_movies:
-        ad.append(movies_ind[movies_ind['movieId']==x]['title'].values)
-    a['Genre_Adventure'] = ad
-for x in top['movieId']:
-    if x in animation_movies:
-        an.append(movies_ind[movies_ind['movieId']==x]['title'].values)
-    a['Genre_Animation'] = an
-for x in top['movieId']:
-    if x in children_movies:
-        ch.append(movies_ind[movies_ind['movieId']==x]['title'].values)
-    a['Genre_Children'] = ch
-for x in top['movieId']:
-    if x in comedy_movies:
-        co.append(movies_ind[movies_ind['movieId']==x]['title'].values)
-    a['Genre_Comedy'] = co
-for e in top['movieId']:
-    if e in fantasy_movies:
-        fa.append(movies_ind[movies_ind['movieId']==e]['title'].values)
-    a['Genre_Fantasy'] = fa
-for f in top['movieId']:
-    if f in romance_movies:
-        ro.append(movies_ind[movies_ind['movieId']==f]['title'].values)
-    a['Genre_Romance'] = ro
-for g in top['movieId']:
-    if g in drama_movies:
-        dr.append(movies_ind[movies_ind['movieId']==g]['title'].values)
-    a['Genre_Drama'] = dr
-for h in top['movieId']:
-    if h in action_movies:
-        ac.append(movies_ind[movies_ind['movieId']==h]['title'].values)
-    a['Genre_Action'] = ac
-for i in top['movieId']:
-    if i in crime_movies:
-        cr.append(movies_ind[movies_ind['movieId']==i]['title'].values)
-    a['Genre_Crime'] = cr
-for j in top['movieId']:
-    if j in thriller_movies:
-        th.append(movies_ind[movies_ind['movieId']==j]['title'].values)
-    a['Genre_Thriller'] = th
-for k in top['movieId']:
-    if k in horror_movies:
-        ho.append(movies_ind[movies_ind['movieId']==k]['title'].values)
-    a['Genre_Horror'] = ho
-for l in top['movieId']:
-    if l in mystery_movies:
-        my.append(movies_ind[movies_ind['movieId']==l]['title'].values)
-    a['Genre_Mystery'] = my
-for m in top['movieId']:
-    if m in scifi_movies:
-        sc.append(movies_ind[movies_ind['movieId']==m]['title'].values)
-    a['Genre_Sci-Fi'] = sc
-for n in top['movieId']:
-    if n in war_movies:
-        wa.append(movies_ind[movies_ind['movieId']==n]['title'].values)
-    a['Genre_War'] = wa
-for o in top['movieId']:
-    if o in musical_movies:
-        mu.append(movies_ind[movies_ind['movieId']==o]['title'].values)
-    a['Genre_Musical'] = mu
-for p in top['movieId']:
-    if p in documentary_movies:
-        do.append(movies_ind[movies_ind['movieId']==p]['title'].values)
-    a['Genre_Documentary'] = do
-for q in top['movieId']:
-    if q in imax_movies:
-        im.append(movies_ind[movies_ind['movieId']==q]['title'].values)
-    a['Genre_IMAX'] = im
-for r in top['movieId']:
-    if r in western_movies:
-        we.append(movies_ind[movies_ind['movieId']==r]['title'].values)
-    a['Genre_Western'] = we
-for s in top['movieId']:
-    if s in noir_movies:
-        fi.append(movies_ind[movies_ind['movieId']==s]['title'].values)
-    a['Genre_Film-Noir'] = fi
-        
+# fills keys with list of movies that belong to respective genre
+for x in genres:
+    li = []
+    for id in top['movieId']:
+        if id in list(movie_genres.loc[movie_genres[x] == 1]['movieId']):
+            li.append(movies_ind[movies_ind['movieId']==id]['title'].values)
+    c[x] = li
 
 
-# In[131]:
+# In[31]:
 
 
-adventure_rec = a['Genre_Adventure'][randint(0, len(a['Genre_Adventure'])-1)][0]
-animation_rec = a['Genre_Animation'][randint(0, len(a['Genre_Animation'])-1)][0]
-children_rec = a['Genre_Children'][randint(0, len(a['Genre_Children'])-1)][0]
-comedy_rec = a['Genre_Comedy'][randint(0, len(a['Genre_Comedy'])-1)][0]
-fantasy_rec = a['Genre_Fantasy'][randint(0, len(a['Genre_Fantasy'])-1)][0]
-romance_rec = a['Genre_Romance'][randint(0, len(a['Genre_Romance'])-1)][0]
-drama_rec = a['Genre_Drama'][randint(0, len(a['Genre_Drama'])-1)][0]
-action_rec = a['Genre_Action'][randint(0, len(a['Genre_Action'])-1)][0]
-crime_rec = a['Genre_Crime'][randint(0, len(a['Genre_Crime'])-1)][0]
-thriller_rec = a['Genre_Thriller'][randint(0, len(a['Genre_Thriller'])-1)][0]
-horror_rec = a['Genre_Horror'][randint(0, len(a['Genre_Horror'])-1)][0]
-mystery_rec = a['Genre_Mystery'][randint(0, len(a['Genre_Mystery'])-1)][0]
-scifi_rec = a['Genre_Sci-Fi'][randint(0, len(a['Genre_Sci-Fi'])-1)][0]
-war_rec = a['Genre_War'][randint(0, len(a['Genre_War'])-1)]
-musical_rec = a['Genre_Musical'][randint(0, len(a['Genre_Musical'])-1)][0]
-imax_rec = a['Genre_IMAX'][randint(0, len(a['Genre_IMAX'])-1)][0]
-western_rec = a['Genre_Western'][randint(0, len(a['Genre_Western'])-1)][0]
-noir_rec = a['Genre_Film-Noir'][randint(0, len(a['Genre_Film-Noir'])-1)][0]
+# dictionary with keys equal to genre
+b = {}
+for x in genres:
+    key = x
+    value = ''
+    b[key] = value 
 
 
-# In[132]:
+# In[32]:
 
 
-genres = np.append(genres, 'none')
+#fills keys with random choice in the list of films within a genre
+for x in genres:
+    if len(c[x])>0:
+        b[x] = c[x][randint(0, len(c[x])-1)][0]
+    else:
+        b[x] = ""
 
 
-# In[133]:
+# In[33]:
+
+
+# add an option for not choosing a genre
+genres_for_q = np.append(genres, 'none')
+
+
+# In[34]:
 
 
 from fuzzywuzzy import process
 
 
-# In[134]:
+# In[35]:
 
 
-genre_answer = process.extractOne(input("What genre of film would you like to watch?"),genres)
+genre_answer = process.extractOne(input("What genre of film would you like to watch?"),genres_for_q)
 
 
-# In[135]:
+# In[41]:
 
 
-if genre_answer[0] == 'Genre_Adventure':
-    if len(a['Genre_Adventure']) == 0:
-        print('No adventure recommendations')
-    else:
-        print('We recommend ' + adventure_rec)
-if genre_answer[0] == 'Genre_Animation':
-    if len(a['Genre_Animation']) == 0:
-        print('No animation recommendations')
-    else:
-        print('We recommend ' + animation_rec)
-if genre_answer[0] == 'Genre_Children':
-    if len(a['Genre_Children']) == 0:
-        print('No chilren\'s movies recommendations')
-    else:
-        print('We recommend ' + children_rec)
-if genre_answer[0] == 'Genre_Comedy':
-    if len(a['Genre_Comedy']) == 0:
-        print('No comedy recommendations')
-    else:
-        print('We recommend ' + comedy_rec)
-if genre_answer[0] == 'Genre_Fantasy':
-    if len(a['Genre_Fantasy']) == 0:
-        print('No fantasty recommendations')
-    else:
-        print('We recommend ' + fantasy_rec)
-if genre_answer[0] == 'Genre_Romance':
-    if len(a['Genre_Romance']) == 0:
-        print('No romance recommendations')
-    else:
-        print('We recommend ' + romance_rec)
-if genre_answer[0] == 'Genre_Drama':
-    if len(a['Genre_Drama']) == 0:
-        print('No drama recommendations')
-    else:
-        print('We recommend ' + drama_rec)
-if genre_answer[0] == 'Genre_Action':
-    if len(a['Genre_Action']) == 0:
-        print('No action recommendations')
-    else:
-        print('We recommend ' + action_rec)
-if genre_answer[0] == 'Genre_Crime':
-    if len(a['Genre_Crime']) == 0:
-        print('No crime recommendations')
-    else:
-        print('We recommend ' + crime_rec)
-if genre_answer[0] == 'Genre_Thriller':
-    if len(a['Genre_Thriller']) == 0:
-        print('No thriller recommendations')
-    else:
-        print('We recommend ' + thriller_rec)
-if genre_answer[0] == 'Genre_Horror':
-    if len(a['Genre_Horror']) == 0:
-        print('No horror recommendations')
-    else:
-        print('We recommend ' + horror_rec)
-if genre_answer[0] == 'Genre_Mystery':
-    if len(a['Genre_Mystery']) == 0:
-        print('No mystery recommendations')
-    else:
-        print('We recommend ' + mystery_rec)
-if genre_answer[0] == 'Genre_Sci-Fi':
-    if len(a['Genre_Sci-Fi']) == 0:
-        print('No sci-fi recommendations')
-    else:
-        print('We recommend ' + scifi_rec)
-if genre_answer[0] == 'Genre_War':
-    if len(a['Genre_War']) == 0:
-        print('No war recommendations')
-    else:
-        print('We recommend ' + war_rec)
-if genre_answer[0] == 'Genre_Musical':
-    if len(a['Genre_Musical']) == 0:
-        print('No musical recommendations')
-    else:
-        print('We recommend ' + musical_rec)
-if genre_answer[0] == 'Genre_Western':
-    if len(a['Genre_Western']) == 0:
-        print('No western recommendations')
-    else:
-        print('We recommend ' + western_rec)
-if genre_answer[0] == 'Genre_Film-Noir':
-    if len(a['Genre_Film-Noir']) == 0:
-        print('No film-noir recommendations')
-    else:
-        print('We recommend ' + noir_rec)
+#picks a top movie of the selected genre
+for x in genres:
+    if genre_answer[0] == x:
+        if len(b[x]) == 0:
+            print('No ' +x+ ' recommedations')
+        else:
+            print('We recommend ' + b[x])
+            
+# if they don't want a specific genre
 if genre_answer[0] == 'none':
-    Select = top_movie_recs[randint(0, 4)]
-    print('We recommend ' + Select)
+        Select = top_movie_recs[randint(0, 4)]
+        print('We recommend ' + Select)
 
